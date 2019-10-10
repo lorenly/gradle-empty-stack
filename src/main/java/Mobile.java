acfa0baf41d1432cbc233d6125cc3e2cca48e93a
LYROL LORENZO (GEDI-ISD-OOCLL/MNL) <LORENLY@OOCL.COM>
2019-10-10 12:22:38 +0800

    Added Mobile class

--- src/main/java/Mobile.java
+++ src/main/java/Mobile.java
@@ -1,4 +1,31 @@
-package PACKAGE_NAME;
-
 public class Mobile {
+    private String name;
+    private String color;
+    private String brand;
+
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return this.name;
+    }
+
+    public void setColor(String color){
+        this.color = color;
+    }
+    public String getColor(){
+        return this.color;
+    }
+
+    public void setBrand(String brand){
+        this.brand = brand;
+    }
+
+    public String getBrand(){
+        return this.brand;
+    }
+
+    public void message(String message){
+        System.out.println("Message : " + message);
+    }
 }
