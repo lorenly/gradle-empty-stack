ad55738058f3e05b2c5f78babbc0911cd0ecfbf0
LYROL LORENZO (GEDI-ISD-OOCLL/MNL) <LORENLY@OOCL.COM>
2019-10-10 18:28:28 +0800

    updated IphoneRobot class

--- src/main/java/User/IPhoneRobot.java
+++ src/main/java/User/IPhoneRobot.java
@@ -1,4 +1,16 @@
 package User;
-
+import Model.Iphone;
+import Model.Mobile;
 public class IPhoneRobot {
+    private String name;
+
+    public IPhoneRobot(String name){
+        this.name = name;
+    }
+
+    public void makeCall(Iphone iphone, String message){
+        iphone.setMessage(message);
+        System.out.println(iphone.getPhoneInformation());
+        System.out.println(iphone.getMessage());
+    }
 }
